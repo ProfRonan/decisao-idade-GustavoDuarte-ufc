653f2171901e7bb5122907d81af6576372767d3a
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-05-18 08:45:22 -0300

    Resolvido

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 a = int(input())
-
 if a < 0:
-    print("impossível!")
-if a < 18:
-    print("não precisa se alistar.")
-if a >= 18 and a <= 65 and a != 64:
-    print("eita!")
-if a == 64:
+   print("impossível!")
+elif a >=0 and a < 18:
+   print("não precisa se alistar.")
+elif a >= 18 and a <= 64:
     print("Não esqueça de votar na próxima eleição.")
-if a >65:
+elif a > 65:
     print("Vá descansar.")
+else:
+    print("eita!")    
